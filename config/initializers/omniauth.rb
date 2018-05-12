2850504eeeaf015db98bbf630d4722e69e886e01
designbyh4ve <designbyh4ve@daum.net>
2018-05-12 08:39:20 +0000

    revise commit

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,5 @@
 OmniAuth.config.logger = Rails.logger
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :google_oauth2, ENV["GOOGLE_CLIENT"], ENV["GOOGLE_SECRET"] 
+  provider :google_oauth2, ENV["12968097008-jtrl8n3hckdj58hccavnthhkeetm45ig.apps.googleusercontent.com"], ENV["uUO2b56h0Ctk52N0w92l3gnj"] 
 end
